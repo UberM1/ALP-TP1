c47ed1838b275b4d765ba56552551383a6453409
Tecla <matiasuberti@letsbit.io>
2024-09-16 13:38:42 -0300

    ej 7

--- src/Eval1.hs
+++ src/Eval1.hs
@@ -14,19 +14,21 @@ type State = M.Map Variable Int
 -- Estado vacío
 -- Completar la definición
 initState :: State
-initState = undefined
+initState = M.empty
 
 -- Busca el valor de una variable en un estado
 -- Completar la definición
 lookfor :: Variable -> State -> Int
-lookfor = undefined
+lookfor v s = f (M.lookup v s)
+  where f (Just x) = x
+        f Nothing = error ("undefined variable " ++ v)
 
 -- Cambia el valor de una variable en un estado
 -- Completar la definición
 update :: Variable -> Int -> State -> State
-update = undefined
+update = M.insert
 
--- Evalúa un programa en el estado vacío
+-- Evalua un programa en el estado nulo
 eval :: Comm -> State
 eval p = stepCommStar p initState
 
@@ -39,9 +41,37 @@ stepCommStar c    s = Data.Strict.Tuple.uncurry stepCommStar $ stepComm c s
 -- Evalúa un paso de un comando en un estado dado
 -- Completar la definición
 stepComm :: Comm -> State -> Pair Comm State
-stepComm = undefined
+stepComm (Let v e) s =  let n :!: s' = evalExp e s
+                        in Skip :!: (update v n s')
+stepComm (Seq Skip c1) s = c1 :!: s
+stepComm (Seq c0 c1) s = let c0' :!: s' = stepComm c0 s
+                         in (Seq c0' c1) :!: s'
+stepComm (IfThenElse b c0 c1) s = let b' :!: s' = evalExp b s
+                                  in if b' then c0 :!: s' else c1 :!: s'
+stepComm (RepeatUntil c b) s = (Seq c (IfThenElse b Skip (RepeatUntil c b))) :!: s
 
 -- Evalúa una expresión
 -- Completar la definición
 evalExp :: Exp a -> State -> Pair a State
-evalExp = undefined
+evalExp (Const nv) s = (nv :!: s)
+evalExp (Var x) s = (lookfor x s) :!: s
+evalExp (UMinus e) s = let n :!: s' = evalExp e s
+                       in (-n) :!: s'
+evalExp (Plus e0 e1) s = evalOp e0 e1 s (+)
+evalExp (Minus e0 e1) s = evalOp e0 e1 s (-)
+evalExp (Times e0 e1) s = evalOp e0 e1 s (*)
+evalExp (Div e0 e1) s = evalOp e0 e1 s div
+evalExp BTrue s = True :!: s
+evalExp BFalse s = False :!: s
+evalExp (Not p) s = let b :!: s' = evalExp p s
+                    in (not b ) :!: s'
+evalExp (Or p0 p1) s =  evalOp p0 p1 s (||)
+evalExp (And p0 p1) s = evalOp p0 p1 s (&&)
+evalExp (Eq e0 e1) s = evalOp e0 e1 s (==)
+evalExp (Lt e0 e1) s =  evalOp e0 e1 s (<)
+evalExp (Gt e0 e1) s = evalOp e0 e1 s (>)
+evalExp (NEq e0 e1) s = evalOp e0 e1 s (/=)
+
+evalOp e0 e1 s f = let n0 :!: s' = evalExp e0 s
+                       n1 :!: s'' = evalExp e1 s'
+                   in (f n0 n1) :!: s''
